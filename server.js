1ee8ad86c253439339553fe280d1cd2646dd5652
yogiram-y <23cs132@nandhaengg.org>
2026-05-15 01:10:31 +0530

    changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 3000;
 
 app.get('/', (req, res) => {
   res.json({
-    message: 'DevOps Exam Project Running Successfully',
+    message: 'DevOppouiytrs Exam Project Running Successfully',
   });
 });
 
